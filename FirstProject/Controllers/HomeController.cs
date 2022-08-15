cbd71b0b4dd7bce32fcdd9552bcf092e17c5f5e1
Katsanakis Georgios <geo.elgeo@gmail.com>
2022-08-15 20:35:14 +0200

    first test app

---
+++ FirstProject/Controllers/HomeController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using FirstProject.Models;
+
+namespace FirstProject.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ViewResult Index()
+        {
+            return View("MyView");
+        }
+    }
+}
