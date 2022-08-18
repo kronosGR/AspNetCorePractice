c7aeb758b7ed3c4b17f07fc1e7543cd825f6cf18
Katsanakis Georgios <geo.elgeo@gmail.com>
2022-08-18 09:56:47 +0200

    enabled MVC

--- LanguageFeatures/Startup.cs
+++ LanguageFeatures/Startup.cs
@@ -16,6 +16,8 @@ namespace LanguageFeatures
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            // enable MVC
+            services.AddControllersWithViews();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -30,10 +32,11 @@ namespace LanguageFeatures
 
             app.UseEndpoints(endpoints =>
             {
-                endpoints.MapGet("/", async context =>
-                {
-                    await context.Response.WriteAsync("Hello World!");
-                });
+                //endpoints.MapGet("/", async context =>
+                //{
+                //    await context.Response.WriteAsync("Hello World!");
+                //});
+                endpoints.MapDefaultControllerRoute();
             });
         }
     }
