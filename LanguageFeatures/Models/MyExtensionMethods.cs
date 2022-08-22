192684d640c39d68f4b25cac97087d415361721a
Katsanakis Georgios <geo.elgeo@gmail.com>
2022-08-22 08:50:01 +0200

    added testing practice

---
+++ LanguageFeatures/Models/MyExtensionMethods.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections.Generic;
+
+namespace LanguageFeatures.Models
+{
+    public static class MyExtensionMethods
+        {
+            public static decimal TotalPrices(this ShoppingCart cartParam)
+            {
+                decimal total = 0;
+                foreach (Product prod in cartParam.Products)
+                {
+                    total += prod?.Price ?? 0;
+                }
+
+                return total;
+            }
+
+            public static IEnumerable<Product> FilterByPrice(this IEnumerable<Product> productEnum,
+                decimal minumumPrice)
+            {
+                foreach (Product prod in productEnum)
+                {
+                    if ((prod?.Price ?? 0) >= minumumPrice)
+                    {
+                        yield return prod;
+                    }
+                }
+            }
+    }
+}
