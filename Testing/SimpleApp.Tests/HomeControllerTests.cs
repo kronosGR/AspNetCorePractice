e6633f805c63eb11fd99bb5d057e55eb78b4fa20
Katsanakis Georgios <geo.elgeo@gmail.com>
2022-08-23 09:00:33 +0200

    one more test

---
+++ Testing/SimpleApp.Tests/HomeControllerTests.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using SimpleApp.Controllers;
+using SimpleApp.Models;
+using Xunit;
+
+namespace SimpleApp.Tests
+{
+    public class HomeControllerTests
+    {
+        [Fact]
+        public void IndexActionModelIsComplete()
+        {
+            var controller = new HomeController();
+            Product[] products = new Product[] {
+                new Product { Name = "Kayak", Price = 275M },
+                new Product { Name = "Lifejacket", Price = 48.95M}
+            };
+
+            var model = (controller.Index() as ViewResult)?.ViewData.Model as IEnumerable<Product>;
+            Assert.Equal(products, model, Comparer.Get<Product>((p1, p2) => p1.Name == p2.Name
+             && p1.Price == p2.Price));
+        }
+    }
+}
