11b5bcb0ca5c9fa28e19657585ce7de397bbfffe
Katsanakis Georgios <geo.elgeo@gmail.com>
2022-08-23 08:51:04 +0200

    added two simple tests

---
+++ Testing/SimpleApp.Tests/ProductTests.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SimpleApp.Models;
+using Xunit;
+
+namespace SimpleApp.Tests
+{
+    public class ProductTests
+    {
+        [Fact]
+        public void CanChangeProductName()
+        {
+            var p = new Product() { Name = "Test", Price = 100M };
+            p.Name = "New name";
+
+            Assert.Equal("New name", p.Name);
+        }
+
+        [Fact]
+        public void CanChangeProductPrice()
+        {
+            var p = new Product() { Name = "Test", Price = 100M };
+            p.Price = 200M;
+            Assert.Equal(200M, p.Price);
+        }
+
+    }
+}
