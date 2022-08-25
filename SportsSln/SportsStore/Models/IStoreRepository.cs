811c06a04fc1e8dc5e708162ee1cf93c74193ad6
Katsanakis Georgios <geo.elgeo@gmail.com>
2022-08-25 09:04:03 +0200

    added a repository

---
+++ SportsSln/SportsStore/Models/IStoreRepository.cs
@@ -0,0 +1,9 @@
+﻿using System.Linq;
+
+namespace SportsStore.Models
+{
+    public class IStoreRepository
+    {
+        IQueryable<Product> Products { get; set; }
+    }
+}
--- SportsSln/SportsStore/Startup.cs
+++ SportsSln/SportsStore/Startup.cs
@@ -34,6 +34,8 @@ namespace SportsStore
                     Configuration["ConnectionStrings:SportsStoreConnection"]);
             });
 
+            services.AddScoped<IStoreRepository, EFStoreRepository>();
+
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
