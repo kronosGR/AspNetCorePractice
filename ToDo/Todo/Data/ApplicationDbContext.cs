b7282dea684d19e213aa8c30540022c5313c21c4
Katsanakis Georgios <geo.elgeo@gmail.com>
2022-08-28 06:50:50 +0200

    added migration and update DB

--- ToDo/Todo/Data/ApplicationDbContext.cs
+++ ToDo/Todo/Data/ApplicationDbContext.cs
@@ -1,10 +1,14 @@
 ﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
+using Todo.Models;
 
 namespace Todo.Data;
 
 public class ApplicationDbContext : IdentityDbContext
 {
+    // represents a table/collection in the dB
+    public DbSet<ToDoItem> Items { get; set; }
+
     public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
         : base(options)
     {
