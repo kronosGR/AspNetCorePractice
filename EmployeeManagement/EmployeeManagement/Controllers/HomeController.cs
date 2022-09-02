1be8e80a0a27ac05fc343c24621704452c9ac6ff
Katsanakis Georgios <geo.elgeo@gmail.com>
2022-09-02 09:06:44 +0200

    added simple view for details

--- EmployeeManagement/EmployeeManagement/Controllers/HomeController.cs
+++ EmployeeManagement/EmployeeManagement/Controllers/HomeController.cs
@@ -19,7 +19,7 @@ namespace EmployeeManagement.Controllers
         public ViewResult Details()
         {
             Employee model = _employeeRepository.GetEmployee(1);
-            return new View(model);
+            return View(model);
         }
     }
 }
