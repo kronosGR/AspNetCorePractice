7205be86344afe37de46ed0b041b3710c707a661
KronosGR <geo.elgeo@gmail.com>
2022-09-13 09:18:47 +0200

    added form validation

--- EmployeeManagement/EmployeeManagement/Controllers/HomeController.cs
+++ EmployeeManagement/EmployeeManagement/Controllers/HomeController.cs
@@ -38,9 +38,14 @@ public class HomeController : Controller
     }
 
     [HttpPost]
-    public RedirectToActionResult Create(Employee employee)
+    public IActionResult Create(Employee employee)
     {
-        var newEmployee = _employeeRepository.Add(employee);
-        return RedirectToAction("Details", new { id = newEmployee.Id });
+        if (ModelState.IsValid)
+        {
+            var newEmployee = _employeeRepository.Add(employee);
+            return RedirectToAction("Details", new { id = newEmployee.Id });
+        }
+
+        return View();
     }
 }
--- EmployeeManagement/EmployeeManagement/Models/Employee.cs
+++ EmployeeManagement/EmployeeManagement/Models/Employee.cs
@@ -1,9 +1,19 @@
-﻿namespace EmployeeManagement.Models;
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace EmployeeManagement.Models;
 
 public class Employee
 {
     public int Id { get; set; }
+
+    [Required]
+    [MaxLength(50, ErrorMessage = "Name cannot exceed 50 characters")]
     public string Name { get; set; }
+
+    [Required]
+    [RegularExpression(@"^([\w\.\-]+)@([\w\-]+)((\.(\w){2,})+)$", ErrorMessage = "Invalid email format")]
+    [Display(Name = "Office Email")]
     public string Email { get; set; }
+
     public Dept Department { get; set; }
 }
