eef01bba8f7aa64feccde1db47d219e7b74f9df1
KronosGR <geo.elgeo@gmail.com>
2022-09-15 10:29:32 +0200

    added sql repository for employee

---
+++ EmployeeManagement/EmployeeManagement/Models/SQLEmployeeRepository.cs
@@ -0,0 +1,48 @@
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+
+namespace EmployeeManagement.Models;
+
+public class SQLEmployeeRepository : IEmployeeRepository
+{
+    private readonly AppDbContext context;
+
+    public SQLEmployeeRepository(AppDbContext context)
+    {
+        this.context = context;
+    }
+
+    public Employee GetEmployee(int Id)
+    {
+        return context.Employees.Find(Id);
+    }
+
+    public IEnumerable<Employee> getAllEmployee()
+    {
+        return context.Employees;
+    }
+
+    public Employee Add(Employee employee)
+    {
+        context.Employees.Add(employee);
+        context.SaveChanges();
+        return employee;
+    }
+
+    public Employee Update(Employee employeeChanges)
+    {
+        var employee = context.Employees.Attach(employeeChanges);
+        employee.State = EntityState.Modified;
+        context.SaveChanges();
+        return employeeChanges;
+    }
+
+    public Employee Delete(int Id)
+    {
+        var employee = context.Employees.Find(Id);
+        if (employee != null) context.Employees.Remove(employee);
+
+        context.SaveChanges();
+        return employee;
+    }
+}
