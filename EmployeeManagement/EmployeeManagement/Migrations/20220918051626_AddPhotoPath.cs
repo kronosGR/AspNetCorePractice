73ff58768f332c3c32b61403a08a2768733b9fa6
KronosGR <geo.elgeo@gmail.com>
2022-09-18 07:22:06 +0200

    added new property to method and updated readme with more info

---
+++ EmployeeManagement/EmployeeManagement/Migrations/20220918051626_AddPhotoPath.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace EmployeeManagement.Migrations
+{
+    public partial class AddPhotoPath : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "PhotoPat",
+                table: "Employees",
+                type: "longtext",
+                nullable: true)
+                .Annotation("MySql:CharSet", "utf8mb4");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "PhotoPat",
+                table: "Employees");
+        }
+    }
+}
---
+++ EmployeeManagement/EmployeeManagement/Migrations/20220918052109_AddPhotoPathColumn.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace EmployeeManagement.Migrations
+{
+    public partial class AddPhotoPathColumn : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "PhotoPath",
+                table: "Employees",
+                type: "longtext",
+                nullable: true)
+                .Annotation("MySql:CharSet", "utf8mb4");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "PhotoPath",
+                table: "Employees");
+        }
+    }
+}
