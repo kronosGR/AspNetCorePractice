8bd33105cf001fd94a6406f151d95ed5c44fb769
KronosGR <geo.elgeo@gmail.com>
2022-09-24 12:26:21 +0200

    use of usermanager and signinmanager

--- EmployeeManagement/EmployeeManagement/Controllers/AccountController.cs
+++ EmployeeManagement/EmployeeManagement/Controllers/AccountController.cs
@@ -1,13 +1,48 @@
+using System.Threading.Tasks;
+using EmployeeManagement.ViewModels;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 
 namespace EmployeeManagement.Controllers;
 
 public class AccountController : Controller
 {
+    private readonly UserManager<IdentityUser> _manager;
+    private readonly SignInManager<IdentityUser> _signInManager;
+
+    public AccountController(
+        UserManager<IdentityUser> userManager,
+        SignInManager<IdentityUser> signInManager
+    )
+    {
+        _signInManager = signInManager;
+        _manager = userManager;
+    }
+
     // GET
     [HttpGet]
     public IActionResult Register()
     {
         return View();
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> Register(RegisterViewModel model)
+    {
+        if (ModelState.IsValid)
+        {
+            var user = new IdentityUser { UserName = model.Email, Email = model.Email };
+            var result = await _manager.CreateAsync(user, model.Password);
+
+            if (result.Succeeded)
+            {
+                await _signInManager.SignInAsync(user, false);
+                return RedirectToAction("Index", "Home");
+            }
+
+            foreach (var error in result.Errors) ModelState.AddModelError("", error.Description);
+        }
+
+        return View(model);
     }
 }
