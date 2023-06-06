a401b419a1cdc87c0c45b92d67b9925926d86992
kronosGR <geo.elgeo@gmail.com>
2023-06-06 11:56:29 +0200

    started FlightPlanApi

--- globo/Api/WebApplicationHouseExtensions.cs
+++ globo/Api/WebApplicationHouseExtensions.cs
@@ -2,50 +2,47 @@
 using Microsoft.AspNetCore.Mvc;
 using MiniValidation;
 
-namespace Api
+public static class WebApplicationHouseExtensions
 {
-    public class WebApplicationHouseExtensions
+    public static void MapHouseEndPoints(this WebApplication app)
     {
-        public static void MapHouseEndPoints(this WebApplication app)
+
+        app.MapGet("/houses", (IHouseRepository repo) => repo.GetAll())
+            .Produces<HouseDto[]>(StatusCodes.Status200OK);
+
+        app.MapGet("/house/{houseId:int}", async (int houseId, IHouseRepository repo) =>
         {
+            var house = await repo.Get(houseId);
+            if (house == null) return Results.Problem($"House with ID {houseId} not found", statusCode: 404);
 
-            app.MapGet("/houses", (IHouseRepository repo) => repo.GetAll())
-                .Produces<HouseDto[]>(StatusCodes.Status200OK);
+            return Results.Ok(house);
+        }).ProducesProblem(404).Produces<HouseDetailDto>(StatusCodes.Status200OK);
 
-            app.MapGet("/house/{houseId:int}", async (int houseId, IHouseRepository repo) =>
-            {
-                var house = await repo.Get(houseId);
-                if (house == null) return Results.Problem($"House with ID {houseId} not found", statusCode: 404);
+        app.MapPost("/houses", async ([FromBody] HouseDetailDto dto, IHouseRepository repo) =>
+        {
+            if (!MiniValidator.TryValidate(dto, out var errors))
+                return Results.ValidationProblem(errors);
+            var newHouse = await repo.Add(dto);
+            return Results.Created($"/house/{newHouse.Id}", newHouse);
+        }).Produces<HouseDetailDto>(StatusCodes.Status201Created).ProducesValidationProblem();
 
-                return Results.Ok(house);
-            }).ProducesProblem(404).Produces<HouseDetailDto>(StatusCodes.Status200OK);
+        app.MapPut("/houses", async ([FromBody] HouseDetailDto dto, IHouseRepository repo) =>
+        {
+            if (!MiniValidator.TryValidate(dto, out var errors))
+                return Results.ValidationProblem(errors);
 
-            app.MapPost("/houses", async ([FromBody] HouseDetailDto dto, IHouseRepository repo) =>
-            {
-                if (!MiniValidator.TryValidate(dto, out var errors))
-                    return Results.ValidationProblem(errors);
-                var newHouse = await repo.Add(dto);
-                return Results.Created($"/house/{newHouse.Id}", newHouse);
-            }).Produces<HouseDetailDto>(StatusCodes.Status201Created).ProducesValidationProblem();
+            if (await repo.Get(dto.Id) == null) return Results.Problem($"House {dto.Id} not found", statusCode: 404);
 
-            app.MapPut("/houses", async ([FromBody] HouseDetailDto dto, IHouseRepository repo) =>
-            {
-                if (!MiniValidator.TryValidate(dto, out var errors))
-                    return Results.ValidationProblem(errors);
+            var updatedHouse = await repo.Update(dto);
+            return Results.Ok(updatedHouse);
+        }).ProducesProblem(404).Produces<HouseDetailDto>(StatusCodes.Status200OK).ProducesValidationProblem();
 
-                if (await repo.Get(dto.Id) == null) return Results.Problem($"House {dto.Id} not found", statusCode: 404);
+        app.MapDelete("/houses/{houseId:int}", async (int houseId, IHouseRepository repo) =>
+        {
+            if (repo.Get(houseId) == null) return Results.Problem($"House {houseId} not found", statusCode: 404);
+            await repo.Delete(houseId);
+            return Results.Ok();
+        }).ProducesProblem(404).Produces(StatusCodes.Status200OK);
 
-                var updatedHouse = await repo.Update(dto);
-                return Results.Ok(updatedHouse);
-            }).ProducesProblem(404).Produces<HouseDetailDto>(StatusCodes.Status200OK).ProducesValidationProblem();
-
-            app.MapDelete("/houses/{houseId:int}", async (int houseId, IHouseRepository repo) =>
-            {
-                if (repo.Get(houseId) == null) return Results.Problem($"House {houseId} not found", statusCode: 404);
-                await repo.Delete(houseId);
-                return Results.Ok();
-            }).ProducesProblem(404).Produces(StatusCodes.Status200OK);
-
-        }
     }
 }
